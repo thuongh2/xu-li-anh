fa1b4b939fae83394457e3cecd241cad99a49fad
thuongh2 <vo.thuong01632985@gmail.com>
2023-05-16 11:24:08 +0700

    check commit

---
+++ Buoc1/GetImageFromVideo.py
@@ -0,0 +1,34 @@
+import cv2
+import time
+
+def main():
+    cap = cv2.VideoCapture('../video/BanTai02.mp4')
+    # Resolution 640*480
+    time.sleep(1)
+    if cap is None or not cap.isOpened():
+        print('Khong the mo file video')
+        return
+    cv2.namedWindow('Image', cv2.WINDOW_AUTOSIZE);
+    n = 1
+    dem = 100
+    while True:
+        [success, img] = cap.read()
+        ch = cv2.waitKey(30)
+        if success:
+            img = cv2.flip(img, 0)
+            img = cv2.rotate(img, cv2.ROTATE_180)
+            imgROI = img[40:(40+480),:] # Tao ra anh 480x480
+
+            imgROI = cv2.resize(imgROI,(320,320))
+            cv2.imshow('Image', imgROI)
+        else:
+            break
+        if n%4 == 0:
+            filename = '../image/BanTai/BanTai_%04d.bmp'%(dem)
+            cv2.imwrite(filename, imgROI)
+            dem = dem + 1
+        n = n + 1
+    return
+
+if __name__ == "__main__":
+    main()
